c78e8e18445b9cd8fb807e8d472e47d4bda600eb
Lycheeicy <49021065+Lycheeicy@users.noreply.github.com>
2021-04-18 22:58:12 -0700

    Add files via upload

--- test.c
+++ test.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    info(1);
+    int result=info(1);
+    printf(1, "Count of processes:%d\n", result);
+    result = info(2);
+    printf(1, "Totall system calls:%d\n", result);
+    result = info(3);
+    printf(1, "Number of pages:%d\n", result);
     exit();
 }
