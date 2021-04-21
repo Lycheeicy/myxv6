3d7ee5c7620f1ade2eb413cf72d80c1a1fa08b88
Lycheeicy <49021065+Lycheeicy@users.noreply.github.com>
2021-04-21 00:12:16 -0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -121,6 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int				info(int);
+void      setticket(int);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
